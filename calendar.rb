63415cf90e22bb278011a543a52d6a52e11814e7
reonm <songcunreon@gmail.com>
2023-03-20 07:37:15 +0900

    3/20

--- calendar.rb
+++ calendar.rb
@@ -8,9 +8,7 @@ opt.on('-m') { |v| p v }
 
 num = ARGV[1].to_i
 
-if (0..12).include?(num)
-  puts num
-else
+if not (1..12).include?(num)
   puts "#{num} is neither a month number (1..12) nor a name"
   return
 end
@@ -18,34 +16,36 @@ end
 require 'date'
 
 year = Date.today.year
-mon = num # 引数でもらってきた値を入れる
-if mon.zero?
-  mon = Date.today.mon # 引数がゼロだった場合は今月を指定する
+# mon = num
+mon = num # 引数でもらってきた値を入れ
+if mon.zero? # 引数がゼロだった場合は今月を指定する
+  mon = Date.today.mon
 end
 title_year = mon.to_s + '月' + year.to_s 
 
-title = Date::MONTHNAMES[mon] + ' ' + title_year # 月と西暦を連結して出力
+title = Date::MONTHNAMES[mon] + " " + title_year # 月と西暦を連結して出力
 
 firstday_wday = Date.new(year, mon, 1).wday   # 今月1日の曜日を取得(0~6)
 lastday_date = Date.new(year, mon, -1).day    # 今月の最終日を取得
 week = %w[Mo Tu We Th Fr Sa Su]
 
+
 puts title.center(20)        # 月と西暦中央寄せで出力
 puts week.join(' ')         # 曜日を出力
 
 if firstday_wday.zero?
-    wday = 0
+  wday = 0
 else 
-    print '   ' * (firstday_wday - 1)
-    wday = firstday_wday - 1
+  print '   ' * (firstday_wday - 1)
+  wday = firstday_wday - 1
 end
 
-(1..lastday_date).each do |date|    # 1~最終日まで繰り返し
-	print "#{date.to_s.rjust(2)} "    # 日付を右寄せで表示
+(1..lastday_date).each do |date|    # 1~最終日まで繰り返し 
+  print "#{date.to_s.rjust(2)} "    # 日付を右寄せで表示
 	wday += 1
 	if (wday % 7).zero?   # 土曜日まで表示したら改行する
-	  print "\n"
+    print "\n"
 	elsif date == lastday_date   # 最後の日付の場合も改行する
-	  print "\n"
+    print "\n"
 	end
 end
